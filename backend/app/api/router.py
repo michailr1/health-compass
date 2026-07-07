f93ad4f9ea255af26344aade899e360419c65a00
michailr1 <76062633+michailr1@users.noreply.github.com>
2026-07-07 17:00:24 +0300

    feat: register session end route

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -7,6 +7,7 @@ from fastapi import APIRouter
 from app.api.routes.auth import router as login_router
 from app.api.routes.health import router as health_router
 from app.api.routes.identity import router as user_router
+from app.api.routes.logout import router as session_end_router
 from app.api.routes.private import router as private_router
 from app.api.routes.provider_login import router as extra_auth_router
 from app.api.routes.version import router as version_router
@@ -19,3 +20,4 @@ api_router.include_router(private_router)
 api_router.include_router(user_router)
 api_router.include_router(login_router)
 api_router.include_router(extra_auth_router)
+api_router.include_router(session_end_router)
