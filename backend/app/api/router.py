0a41b9b6d77537f0a889aad1f5cf10d9942989ff
michailr1 <76062633+michailr1@users.noreply.github.com>
2026-07-07 13:29:51 +0300

    feat: register additional auth route

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -8,6 +8,7 @@ from app.api.routes.auth import router as login_router
 from app.api.routes.health import router as health_router
 from app.api.routes.identity import router as user_router
 from app.api.routes.private import router as private_router
+from app.api.routes.provider_login import router as extra_auth_router
 from app.api.routes.version import router as version_router
 
 api_router = APIRouter()
@@ -17,3 +18,4 @@ api_router.include_router(version_router)
 api_router.include_router(private_router)
 api_router.include_router(user_router)
 api_router.include_router(login_router)
+api_router.include_router(extra_auth_router)
