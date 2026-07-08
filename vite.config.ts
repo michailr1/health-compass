a61ad601f08acdc9fca02e2bf65e59cca3d10073
michailr1 <76062633+michailr1@users.noreply.github.com>
2026-07-09 00:46:41 +0300

    feat: serve frontend from subdomain root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/health/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
